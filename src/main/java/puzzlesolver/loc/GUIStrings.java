5da1b903bf2220527ec7f69a4ecf9952ad1214e5
PTRCoder <pj.de.bruijne@gmail.com>
2026-03-25 15:47:32 +0100

    Added new default entry point for localized GUI strings
    Signed-off-by: PTRCoder <pj.de.bruijne@gmail.com>

---
+++ src/main/java/puzzlesolver/loc/GUIStrings.java
@@ -0,0 +1,43 @@
+package puzzlesolver.loc;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableStringValue;
+import org.jetbrains.annotations.NonNls;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public final class GUIStrings {
+    @NonNls
+    private static final String BUNDLE_NAME = "gui";
+    private static final Property<ResourceBundle> RESOURCE_BUNDLE_PROPERTY =
+            new SimpleObjectProperty<>(ResourceBundle.getBundle(BUNDLE_NAME, Locale.ENGLISH));
+
+    public static final ObservableStringValue TITLE = StringProperty.stringExpression(
+            RESOURCE_BUNDLE_PROPERTY.map(x -> x.getString("title"))
+    );
+    public static final ObservableStringValue FILE_MENU_NAME = StringProperty.stringExpression(
+            RESOURCE_BUNDLE_PROPERTY.map(x -> x.getString("menu.file"))
+    );
+    public static final ObservableStringValue EDIT_MENU_NAME = StringProperty.stringExpression(
+            RESOURCE_BUNDLE_PROPERTY.map(x -> x.getString("menu.edit"))
+    );
+    public static final ObservableStringValue PUZZLE_MENU_NAME = StringProperty.stringExpression(
+            RESOURCE_BUNDLE_PROPERTY.map(x -> x.getString("menu.puzzle"))
+    );
+    public static final ObservableStringValue SETTINGS_MENU_NAME = StringProperty.stringExpression(
+            RESOURCE_BUNDLE_PROPERTY.map(x -> x.getString("menu.settings"))
+    );
+
+    private GUIStrings() {}
+
+    public static void updateLocale(Locale locale) {
+        RESOURCE_BUNDLE_PROPERTY.setValue(ResourceBundle.getBundle(BUNDLE_NAME, locale));
+    }
+    
+    public static Locale getLocale() {
+        return RESOURCE_BUNDLE_PROPERTY.getValue().getLocale();
+    }
+}
---
+++ src/main/java/puzzlesolver/loc/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package puzzlesolver.loc;
+
+import org.jspecify.annotations.NullMarked;
