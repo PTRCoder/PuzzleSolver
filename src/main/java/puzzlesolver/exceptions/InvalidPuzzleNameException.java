bc5a7eb8902ce00d01386480e7a485003f2c0d72
PTRCoder <pj.de.bruijne@gmail.com>
2026-03-25 15:46:09 +0100

    Created no-args constructor with default message
    Signed-off-by: PTRCoder <pj.de.bruijne@gmail.com>

--- src/main/java/puzzlesolver/exceptions/InvalidPuzzleNameException.java
+++ src/main/java/puzzlesolver/exceptions/InvalidPuzzleNameException.java
@@ -1,7 +1,12 @@
 package puzzlesolver.exceptions;
 
 public class InvalidPuzzleNameException extends Exception {
+    private static final String DEFAULT_MESSAGE = "This name cannot be resolved";
     public InvalidPuzzleNameException(String message) {
         super(message);
+    }
+
+    public InvalidPuzzleNameException() {
+        super(DEFAULT_MESSAGE);
     }
 }
