718777881177f609e82d9afe04b8e46f77d40153
PTRCoder <pj.de.bruijne@gmail.com>
2026-04-12 15:10:55 +0200

    Added automatic creation of default puzzles location
    Signed-off-by: PTRCoder <pj.de.bruijne@gmail.com>

--- src/main/java/puzzlesolver/fs/FileSystem.java
+++ src/main/java/puzzlesolver/fs/FileSystem.java
@@ -1,12 +1,25 @@
 package puzzlesolver.fs;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 
+@Slf4j
 @UtilityClass
 public class FileSystem {
     public static final File WORKING_FOLDER = new File(System.getProperty("user.dir"));
     public static final File DEFAULT_PUZZLES_FOLDER = WORKING_FOLDER.toPath().resolve("puzzles").toFile();
     public static final File CONFIG_FILE = WORKING_FOLDER.toPath().resolve("config.toml").toFile();
+
+    // If DEFAULT_PUZZLES_FOLDER does not exist, create it and log it
+    static {
+        final boolean created;
+        if (!DEFAULT_PUZZLES_FOLDER.exists())
+            created = DEFAULT_PUZZLES_FOLDER.mkdir();
+        else
+            created = false;
+        if (created)
+            log.info("Created default puzzle folder at {}", DEFAULT_PUZZLES_FOLDER);
+    }
 }
