5f3fb7d11d285b8ba1119c3bba3d0c076bb0d189
PTRCoder <pj.de.bruijne@gmail.com>
2026-04-25 20:02:13 +0200

    Created base class for FillValue cells ui
    This base class should make it easier to migrate functionality of one cell ui to another puzzle type. It also handles most of the details regarding the FillValue cells value updates.
    
    Signed-off-by: PTRCoder <pj.de.bruijne@gmail.com>

--- src/main/java/puzzlesolver/puzzles/kakurasu/ui/KakurasuCellUI.java
+++ src/main/java/puzzlesolver/puzzles/kakurasu/ui/KakurasuCellUI.java
@@ -1,13 +1,15 @@
 package puzzlesolver.puzzles.kakurasu.ui;
 
-import javafx.scene.control.CheckBox;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NonNls;
 import puzzlesolver.generics.puzzle.FillValue;
 import puzzlesolver.puzzles.kakurasu.puzzle.KakurasuCell;
+import puzzlesolver.ui.FillValueCellUI;
 
 @Slf4j
-public class KakurasuCellUI extends CheckBox {
+public class KakurasuCellUI extends FillValueCellUI {
     private static final @NonNls String CSS = "kakurasu-cell";
 
     public KakurasuCellUI(KakurasuCell data) {
@@ -16,11 +18,11 @@ public class KakurasuCellUI extends CheckBox {
         this.getStyleClass().add(CSS);
         this.applyCss();
 
-        this.setMouseTransparent(true);
-        this.focusTraversableProperty().set(false);
-        this.selectedProperty().bind(data.valueProperty().isEqualTo(FillValue.CROSSED));
-        this.indeterminateProperty().bind(data.valueProperty().isEqualTo(FillValue.FILLED));
-//        this.textProperty().bind(this.selectedProperty().map(Object::toString));
+        this.valueProperty().bindBidirectional(data.valueProperty());
     }
 
+    @Override
+    protected Node conversion(FillValue value) {
+        return new Label(value.toText());
+    }
 }
---
+++ src/main/java/puzzlesolver/ui/FillValueCellUI.java
@@ -0,0 +1,83 @@
+package puzzlesolver.ui;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Node;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.Skin;
+import javafx.scene.control.skin.LabeledSkinBase;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import puzzlesolver.generics.puzzle.FillValue;
+
+@EqualsAndHashCode(callSuper = true)
+@ToString
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Getter
+@Accessors(fluent = true)
+@Slf4j
+public abstract class FillValueCellUI extends Labeled {
+    Property<FillValue> valueProperty = new SimpleObjectProperty<>(FillValue.EMPTY);
+    BooleanProperty editableProperty = new SimpleBooleanProperty();
+
+    public FillValueCellUI() {
+        super();
+        this.setOnMouseReleased(this::input);
+        this.graphicProperty().bind(valueProperty.map(this::conversion));
+        this.editableProperty().setValue(true);
+    }
+
+    protected abstract Node conversion(FillValue value);
+
+    private void input(MouseEvent e) {
+        if (editableProperty().get()) {
+            MouseButton mb = e.getButton();
+            log.debug("{}", mb);
+            switch (mb) {
+                case PRIMARY -> traverseL(valueProperty());
+                case SECONDARY -> traverseR(valueProperty());
+            }
+        }
+    }
+
+    private static void traverseL(Property<FillValue> prop) {
+        FillValue old = prop.getValue();
+        FillValue n = switch (old) {
+            case EMPTY, CROSSED -> FillValue.FILLED;
+            case FILLED -> FillValue.EMPTY;
+            default -> throw new IllegalArgumentException();
+        };
+        prop.setValue(n);
+    }
+
+    private static void traverseR(Property<FillValue> prop) {
+        FillValue old = prop.getValue();
+        FillValue n = switch (old) {
+            case EMPTY, FILLED -> FillValue.CROSSED;
+            case CROSSED -> FillValue.EMPTY;
+            default -> throw new IllegalArgumentException();
+        };
+        prop.setValue(n);
+    }
+
+    @Override
+    protected Skin<?> createDefaultSkin() {
+        return new FillValueCellUISkin(this);
+    }
+
+    private static class FillValueCellUISkin extends LabeledSkinBase<FillValueCellUI> {
+
+        public FillValueCellUISkin(FillValueCellUI labeled) {
+            super(labeled);
+        }
+    }
+}
