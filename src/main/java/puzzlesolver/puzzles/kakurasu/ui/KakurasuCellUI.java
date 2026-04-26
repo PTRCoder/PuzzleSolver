bd2897ee2fcbb1b28eaf044c198fb297f5388fda
PTRCoder <pj.de.bruijne@gmail.com>
2026-04-26 20:27:46 +0200

    Updated cell ui
    This update allows for easier use of clicking and dragging for setting the state of cells. Furthermore, it makes the process of switching states easier by simply setting the visibility instead of creating and destroying nodes.
    
    Signed-off-by: PTRCoder <pj.de.bruijne@gmail.com>

--- src/main/java/puzzlesolver/puzzles/kakurasu/ui/KakurasuCellUI.java
+++ src/main/java/puzzlesolver/puzzles/kakurasu/ui/KakurasuCellUI.java
@@ -1,14 +1,14 @@
 package puzzlesolver.puzzles.kakurasu.ui;
 
 import javafx.scene.Node;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Paint;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NonNls;
-import org.jspecify.annotations.Nullable;
-import puzzlesolver.generics.puzzle.FillValue;
 import puzzlesolver.puzzles.kakurasu.puzzle.KakurasuCell;
 import puzzlesolver.ui.FillValueCellUI;
 
@@ -17,45 +17,44 @@ public class KakurasuCellUI extends FillValueCellUI {
     private static final @NonNls String CSS = "kakurasu-cell";
     private static final int CROSSED_OFFSET = 10;
     private static final double CROSSED_WIDTH = 3;
-    private static final Paint RED = Paint.valueOf("red");
+    private static final Background BG_INVALID = new Background(new BackgroundFill(Color.YELLOW, null, null));
 
     public KakurasuCellUI(KakurasuCell data) {
         super();
         this.getStyleClass().add(CSS);
 
         this.valueProperty().bindBidirectional(data.valueProperty());
+        this.backgroundProperty().bind(data.validProperty().map(x -> x ? null : BG_INVALID));
+        this.editableProperty().bind(data.lockedProperty().not());
     }
 
     @Override
-    protected @Nullable Node conversion(FillValue value) {
-        return switch (value) {
-            case EMPTY, BLOCKED -> null;
-            case CROSSED -> {
-                Line l1 = new Line();
-                l1.startXProperty().set(CROSSED_OFFSET);
-                l1.startYProperty().set(CROSSED_OFFSET);
-                l1.endXProperty().bind(this.widthProperty().subtract(CROSSED_OFFSET));
-                l1.endYProperty().bind(this.heightProperty().subtract(CROSSED_OFFSET));
-                l1.setStrokeWidth(CROSSED_WIDTH);
-                l1.setStroke(RED);
-                Line l2 = new Line();
-                l2.startXProperty().bind(l1.startXProperty());
-                l2.startYProperty().bind(l1.endYProperty());
-                l2.endXProperty().bind(l1.endXProperty());
-                l2.endYProperty().bind(l1.startYProperty());
-                l2.setStrokeWidth(CROSSED_WIDTH);
-                l2.setStroke(RED);
+    protected Node createFilled() {
+        Rectangle s = new Rectangle();
+        s.heightProperty().bind(this.heightProperty().subtract(5));
+        s.widthProperty().bind(this.widthProperty().subtract(5));
+        return s;
+    }
 
-                StackPane cross = new StackPane();
-                cross.getChildren().addAll(l1, l2);
-                yield cross;
-            }
-            case FILLED -> {
-                Rectangle s = new Rectangle();
-                s.heightProperty().bind(this.heightProperty().subtract(5));
-                s.widthProperty().bind(this.widthProperty().subtract(5));
-                yield s;
-            }
-        };
+    @Override
+    protected Node createCrossed() {
+        Line l1 = new Line();
+        l1.startXProperty().set(CROSSED_OFFSET);
+        l1.startYProperty().set(CROSSED_OFFSET);
+        l1.endXProperty().bind(this.widthProperty().subtract(CROSSED_OFFSET));
+        l1.endYProperty().bind(this.heightProperty().subtract(CROSSED_OFFSET));
+        l1.setStrokeWidth(CROSSED_WIDTH);
+        l1.setStroke(Color.RED);
+        Line l2 = new Line();
+        l2.startXProperty().bind(l1.startXProperty());
+        l2.startYProperty().bind(l1.endYProperty());
+        l2.endXProperty().bind(l1.endXProperty());
+        l2.endYProperty().bind(l1.startYProperty());
+        l2.setStrokeWidth(CROSSED_WIDTH);
+        l2.setStroke(Color.RED);
+
+        StackPane cross = new StackPane();
+        cross.getChildren().addAll(l1, l2);
+        return cross;
     }
 }
--- src/main/java/puzzlesolver/ui/FillValueCellUI.java
+++ src/main/java/puzzlesolver/ui/FillValueCellUI.java
@@ -10,6 +10,7 @@ import javafx.scene.control.Skin;
 import javafx.scene.control.skin.LabeledSkinBase;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,6 +21,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.Nullable;
 import puzzlesolver.generics.puzzle.FillValue;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 @EqualsAndHashCode(callSuper = true)
 @ToString
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -27,47 +30,72 @@ import puzzlesolver.generics.puzzle.FillValue;
 @Accessors(fluent = true)
 @Slf4j
 public abstract class FillValueCellUI extends Labeled {
+    private static final AtomicReference<@Nullable FillValue> replacement = new AtomicReference<>(null);
+
+    @Getter(AccessLevel.NONE)
+    Node filled = createFilled();
+    @Getter(AccessLevel.NONE)
+    Node crossed = createCrossed();
+
     Property<FillValue> valueProperty = new SimpleObjectProperty<>(FillValue.EMPTY);
     BooleanProperty editableProperty = new SimpleBooleanProperty();
 
     public FillValueCellUI() {
         super();
-        this.setOnMouseReleased(this::input);
-        this.graphicProperty().bind(valueProperty.map(this::conversion));
+        this.setOnMousePressed(this::input);
+        this.setOnDragDetected(e -> this.startFullDrag());
+        this.setOnMouseDragEntered(this::inputDrag);
+        this.setOnMouseDragReleased(e -> replacement.set(null));
+        StackPane pane = new StackPane();
+        pane.getChildren().addAll(filled, crossed);
+        this.setGraphic(pane);
         this.editableProperty().setValue(true);
+        crossed.visibleProperty().bind(valueProperty().map(x -> x == FillValue.CROSSED));
+        filled.visibleProperty().bind(valueProperty().map(x -> x == FillValue.FILLED));
     }
 
-    protected abstract @Nullable Node conversion(FillValue value);
+    protected abstract Node createFilled();
+
+    protected abstract Node createCrossed();
 
     private void input(MouseEvent e) {
         if (editableProperty().get()) {
             MouseButton mb = e.getButton();
             log.debug("{}", mb);
+            FillValue old = valueProperty().getValue();
+            FillValue n;
             switch (mb) {
-                case PRIMARY -> traverseL(valueProperty());
-                case SECONDARY -> traverseR(valueProperty());
+                case PRIMARY -> n = traverseL(old);
+                case SECONDARY -> n = traverseR(old);
+                default -> {return;}
             }
+            valueProperty().setValue(n);
+            replacement.set(n);
         }
     }
 
-    private static void traverseL(Property<FillValue> prop) {
-        FillValue old = prop.getValue();
-        FillValue n = switch (old) {
+    private void inputDrag(MouseEvent e) {
+        FillValue rep = replacement.get();
+        if (editableProperty().get() && rep != null) {
+            log.debug("{}", rep);
+            valueProperty().setValue(rep);
+        }
+    }
+
+    private static FillValue traverseL(FillValue old) {
+        return switch (old) {
             case EMPTY, CROSSED -> FillValue.FILLED;
             case FILLED -> FillValue.EMPTY;
             default -> throw new IllegalArgumentException();
         };
-        prop.setValue(n);
     }
 
-    private static void traverseR(Property<FillValue> prop) {
-        FillValue old = prop.getValue();
-        FillValue n = switch (old) {
+    private static FillValue traverseR(FillValue old) {
+        return switch (old) {
             case EMPTY, FILLED -> FillValue.CROSSED;
             case CROSSED -> FillValue.EMPTY;
             default -> throw new IllegalArgumentException();
         };
-        prop.setValue(n);
     }
 
     @Override
